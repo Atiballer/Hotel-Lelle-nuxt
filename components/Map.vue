f9aa5d66f6e950b9d1654591d24283c94f73bdce
Sárik Attila <sarik.attila@gmail.com>
2018-11-30 19:07:43 +0100

    marker

--- components/Map.vue
+++ components/Map.vue
@@ -6,7 +6,7 @@
              style="width: 100%; max-width: 600px; height: 400px; margin: auto">
       <GmapMarker :key="index"
                   v-for="(m, index) in markers"
-                  :position="{ lat:46.7886167, lng:17.681446 }"
+                  :position="{ lat:46.786883, lng:17.685008 }"
                   :clickable="true"
                   :draggable="true"
                   @click="center=m.position" />
